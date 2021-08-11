65d3fa0016329169871e1332cb40f090d3055a64
bf2344 <bford2344@gmail.com>
2021-08-11 07:16:04 -0600

    took out menu group

--- src/components/SideBar/SideBarNav/SideBarNav.tsx
+++ src/components/SideBar/SideBarNav/SideBarNav.tsx
@@ -13,34 +13,6 @@ interface Props {
 const SidebarNav = ({ pages, onClose, className, ...rest }: Props): JSX.Element => {
     const classes = useStyles();
 
-    const MenuGroup = ({ item }: MenuGroupProps): JSX.Element => (
-        <List disablePadding>
-            <ListItem disableGutters>
-                <Typography
-                    variant="body2"
-                    color="primary"
-                    className={classes.menuGroupTitle}
-                >
-                    {item.groupTitle}
-                </Typography>
-            </ListItem>
-            {item.pages.map((page, i) => (
-                <ListItem disableGutters key={i} className={classes.menuGroupItem}>
-                    <Typography
-                        variant="body2"
-                        component={'a'}
-                        href={page.href}
-                        className={clsx(classes.navLink, 'submenu-item')}
-                        color="textPrimary"
-                        onClick={() => onClose()}
-                    >
-                        {page.title}
-                    </Typography>
-                </ListItem>
-            ))}
-        </List>
-    );
-
     return (
         <List {...rest} className={clsx(classes.root, className)}>
             <ListItem className={classes.listItem}>
